8322f4574583502aea10fee51c724c8335c8cc7f
tatebonham <tatebonham@gmail.com>
2022-08-19 16:06:23 -0700

    Improved the movement by adding a speed object that the keyDown event changes

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 
 class Entity{
-    constructor(x, y, width, height, color){
+    constructor(x, y, width, height, color, {speed}){
         this.x = x
         this.y = y
         this.width = width
         this.height = height
         this.color = color
+        this.speed = speed
         this.alive = true
     }
 
@@ -25,21 +26,29 @@ class Entity{
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
+
+    update(){
+        this.spawn()
+        this.x += this.speed.x
+        this.y += this.speed.y
+    }
+
+
 }
 
-const adventurer = new Entity(10, 300, 25, 25, 'green')
-adventurer.spawn()
+const adventurer = new Entity(10, 300, 25, 25, 'green',{speed: {x: 0, y: 0}})
+// adventurer.spawn()
 
-const levelOne = ()=>{
-const goblinA = new Entity(300, 300, 25, 25, 'red')
-goblinA.spawn()
-const goblinB = new Entity(200, 230, 25, 25, 'red')
-goblinB.spawn()
-const goblinC = new Entity(400, 400, 25, 25, 'red')
-goblinC.spawn()
-const goblinD = new Entity(500, 100, 25, 25, 'red')
-goblinD.spawn()
-}
+// const levelOne = ()=>{
+// const goblinA = new Entity(300, 300, 25, 25, 'red')
+// goblinA.spawn()
+// const goblinB = new Entity(200, 230, 25, 25, 'red')
+// goblinB.spawn()
+// const goblinC = new Entity(400, 400, 25, 25, 'red')
+// goblinC.spawn()
+// const goblinD = new Entity(500, 100, 25, 25, 'red')
+// goblinD.spawn()
+// }
 
 
 function animate(){
@@ -47,34 +56,34 @@ function animate(){
     ctx.fillStyle = 'gray'
     ctx.fillRect(0,0, canvas.width, canvas.height)
     // console.log('go')
-    adventurer.spawn()
-    levelOne()
+    adventurer.update()
+    // levelOne()
 }
 animate()
 
 window.addEventListener('keydown', (event) => {
-    const playerSpeed = 10
+    const playerSpeed = 1
     switch(event.key){
         case 'w':
-            adventurer.y -= playerSpeed
+            adventurer.speed.y = -1
             if(adventurer.y < 0){
                 adventurer.y = 0
             }
             break
         case 'a':
-            adventurer.x -= playerSpeed
+            adventurer.speed.x = -1
             if(adventurer.x < 0){
                 adventurer.x = 0
             }
             break
         case 's':
-            adventurer.y += playerSpeed
+            adventurer.speed.y = 1
             if(adventurer.y  + adventurer.height > canvas.height){
                 adventurer.y = canvas.height - adventurer.height
             }
             break
         case 'd':
-            adventurer.x += playerSpeed
+            adventurer.speed.x = 1
             if(adventurer.x + adventurer.width > canvas.width){
                 adventurer.x = canvas.width - adventurer.width
             }
