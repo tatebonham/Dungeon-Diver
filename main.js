e178805f93d6480ecec20df28d2e30e2ce0a18cc
tatebonham <tatebonham@gmail.com>
2022-08-19 12:49:10 -0700

    created a class and function to create entities

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ const ctx = canvas.getContext('2d')
 
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
+
+class Entity{
+    constructor(x, y, width, height, color){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.color = color
+        this.alive = true
+    }
+
+
+    spawn(){
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+}
